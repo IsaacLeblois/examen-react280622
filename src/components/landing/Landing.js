01bf59a4e938b33118ba514636c6089eb5265b13
Isaac Leblois <isaacleblois@gmail.com>
2022-06-28 19:41:24 -0500

    Landing Update

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -4,9 +4,15 @@ import './landing.css'
 
 const Landing = () => {
   return (
-    <div className='d-flex flex-column align-items-center landingCont'>
+    <div>
+      <div className='d-flex flex-column align-items-center landingCont'>
         <h1>¡Log in now!</h1>
         <Link className="btn btn-danger w-25 p-2 m-4" to='/login'>Login</Link>
+      </div>
+      <div>
+        <p className='text-center'>Thanks for review my project</p>
+        <p className='text-center fw-bold'>Isaac Vázquez - Full Stack Developer</p>
+      </div>
     </div>
   )
 }
